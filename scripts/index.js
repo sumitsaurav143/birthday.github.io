e9dbc9f5cfadf37ed24f173274c1acd3bdf80870
SUMIT SAURAV <52030057+sumitsaurav143@users.noreply.github.com>
2021-07-06 23:05:05 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'July 7, 2020',
+  birthdate: 'July 7, 2021',
   name: 'Chotuu'
 };
 
